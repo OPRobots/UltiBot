f57818fb52e1007a1c41673f5b9f1e3eb8118a31
robotaleh <alex.santos.ete@gmail.com>
2023-04-07 18:59:33 +0200

    Añade funciones básicas de botones

---
+++ source_code/src/buttons.c
@@ -0,0 +1,50 @@
+#include "buttons.h"
+#include "delay.h"
+
+/**
+ * @brief Obtiene el estado del botón de inicio
+ * 
+ * @return bool
+ */
+bool get_start_btn() {
+  bool state1 = (bool)gpio_get(GPIOB, GPIO12);
+  delay(50);
+  bool state2 = (bool)gpio_get(GPIOB, GPIO12);
+  return state1 && state2;
+}
+
+/**
+ * @brief Obtiene el estado del botón de Menú Arriba
+ * 
+ * @return bool
+ */
+bool get_menu_up_btn() {
+  bool state1 = (bool)gpio_get(GPIOB, GPIO15);
+  delay(50);
+  bool state2 = (bool)gpio_get(GPIOB, GPIO15);
+  return state1 && state2;
+}
+
+/**
+ * @brief Obtiene el estado del botón de Menú Abajo
+ * 
+ * @return bool
+ */
+bool get_menu_down_btn() {
+  bool state1 = (bool)gpio_get(GPIOB, GPIO13);
+  delay(50);
+  bool state2 = (bool)gpio_get(GPIOB, GPIO13);
+  return state1 && state2;
+}
+
+/**
+ * @brief Obtiene el estado del botón de Menú Modo
+ * 
+ * @return bool
+ */
+bool get_menu_mode_btn() {
+  bool state1 = (bool)gpio_get(GPIOB, GPIO14);
+  delay(50);
+  bool state2 = (bool)gpio_get(GPIOB, GPIO14);
+  return state1 && state2;
+}
---
+++ source_code/src/buttons.h
@@ -0,0 +1,15 @@
+#ifndef __BUTTONS_H
+#define __BUTTONS_H
+
+#include "config.h"
+#include "delay.h"
+#include "math.h"
+#include "stdio.h"
+#include <libopencm3/stm32/gpio.h>
+
+bool get_start_btn(void);
+bool get_menu_up_btn(void);
+bool get_menu_down_btn(void);
+bool get_menu_mode_btn(void);
+
+#endif
