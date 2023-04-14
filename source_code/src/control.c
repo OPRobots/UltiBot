91ea01dd39cb23ac11aeb8fb97d56da30da0620f
robotaleh <alex.santos.ete@gmail.com>
2023-04-14 18:05:53 +0200

    Añade estructura de estrategias

--- source_code/src/control.c
+++ source_code/src/control.c
@@ -1,6 +1,7 @@
 #include "control.h"
 
 static bool competicionIniciada = false;
+static enum STRATS current_strat;
 
 /**
  * @brief Comprueba si el robot está en funcionamiento
@@ -23,18 +24,45 @@ void set_competicion_iniciada(bool state) {
 }
 
 /**
+ * @brief Estrategia simple evitando salirse del dohyo, ignorando valor de sensores
+ *
+ */
+static void strat_keeping_inside(void) {
+  set_motors_speed(50, 50);
+  if (get_sensor_calibrated(SENSOR_LINE_LEFT) < LINE_SENSOR_THRESHOLD || get_sensor_calibrated(SENSOR_LINE_RIGHT) < LINE_SENSOR_THRESHOLD) {
+    set_motors_speed(-100, -100);
+    delay(200);
+    set_motors_speed(100, -100);
+    delay(200);
+  }
+}
+
+/**
+ * @brief Establece la estrategia actual
+ *
+ * @param STRATS strat
+ */
+void set_strat(enum STRATS strat) {
+  current_strat = strat;
+}
+
+/**
  * @brief Función principal de control que se llama en el Systick cada 1ms
+ * TODO: mover a ISR independiente para no perjudicar la gestión de los leds
  *
  */
 void control_main_loop(void) {
   if (is_competicion_iniciada()) {
-    set_motors_speed(50, 50);
-    if (get_sensor_calibrated(SENSOR_LINE_LEFT) < LINE_SENSOR_THRESHOLD || get_sensor_calibrated(SENSOR_LINE_RIGHT) < LINE_SENSOR_THRESHOLD) {
-      set_motors_speed(-50, -50);
-      delay(200);
-      set_motors_speed(-50, 50);
-      delay(300);
+    switch (current_strat) {
+      case STRAT_KEEPING_INSIDE:
+        strat_keeping_inside();
+        break;
+      default:
+        set_fatal_error_rgb();
+        set_motors_speed(0, 0);
+        break;
     }
+
   } else {
     set_motors_speed(0, 0);
   }
--- source_code/src/control.h
+++ source_code/src/control.h
@@ -8,8 +8,14 @@
 #include "stdio.h"
 #include <libopencm3/stm32/gpio.h>
 
+enum STRATS {
+  STRAT_KEEPING_INSIDE = 0
+};
+
 bool is_competicion_iniciada(void);
 void set_competicion_iniciada(bool state);
+
+void set_strat(enum STRATS strat);
 void control_main_loop(void);
 
 #endif
