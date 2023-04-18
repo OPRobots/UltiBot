ad6260a9713a9e009ac44703da371b4f1f1a2d7d
robotaleh <alex.santos.ete@gmail.com>
2023-04-18 18:26:51 +0200

    Simplifica la gestión de los motores

--- source_code/src/motors.c
+++ source_code/src/motors.c
@@ -6,17 +6,17 @@ void set_motors_speed(float velI, float velD) {
 
   if (velI >= 0) {
     timer_set_oc_value(TIM4, TIM_OC3, MOTORES_MAX_PWM);
-    timer_set_oc_value(TIM4, TIM_OC4, MOTORES_MAX_PWM - map(velI, 0, 100, 0, MOTORES_MAX_PWM));
+    timer_set_oc_value(TIM4, TIM_OC4, MOTORES_MAX_PWM - MOTORES_MAX_PWM * velD / 100);
   } else {
-    timer_set_oc_value(TIM4, TIM_OC3, MOTORES_MAX_PWM - map(abs(velI), 0, 100, 0, MOTORES_MAX_PWM));
+    timer_set_oc_value(TIM4, TIM_OC3, MOTORES_MAX_PWM - MOTORES_MAX_PWM * abs(velD) / 100));
     timer_set_oc_value(TIM4, TIM_OC4, MOTORES_MAX_PWM);
   }
 
   if (velD >= 0) {
     timer_set_oc_value(TIM4, TIM_OC1, MOTORES_MAX_PWM);
-    timer_set_oc_value(TIM4, TIM_OC2, MOTORES_MAX_PWM - map(velI, 0, 100, 0, MOTORES_MAX_PWM));
+    timer_set_oc_value(TIM4, TIM_OC2, MOTORES_MAX_PWM - MOTORES_MAX_PWM * velI / 100);
   } else {
-    timer_set_oc_value(TIM4, TIM_OC1, MOTORES_MAX_PWM - map(abs(velI), 0, 100, 0, MOTORES_MAX_PWM));
+    timer_set_oc_value(TIM4, TIM_OC1, MOTORES_MAX_PWM - MOTORES_MAX_PWM * abs(velI) / 100);
     timer_set_oc_value(TIM4, TIM_OC2, MOTORES_MAX_PWM);
   }
 }
