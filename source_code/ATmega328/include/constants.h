5070c1ab4ff8d4eccaeea3c11d6693b87f06113a
robotaleh <alex.santos.ete@gmail.com>
2023-09-05 18:50:20 +0200

    Elimina la variante ATmega328

--- source_code/ATmega328/include/constants.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-#define MAX_PWM 255
-#define MILLIS_INICIO 5000
-
-//TODO: PID??
-
-#endif
--- source_code/ATmega328/src/motors.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOTORS_H
-#define MOTORS_H
-
-#include "commons.h"
-#include "constants.h"
-
-void set_speed(int velD, int velI);
-
-#endif
